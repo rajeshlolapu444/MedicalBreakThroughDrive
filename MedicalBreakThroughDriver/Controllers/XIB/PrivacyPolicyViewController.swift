9eca70f0f8d239e6de7f74f1e46fd9c9cb5b8fa1
rajeshlolapu444 <rajeshlolapu@webbaysys.com>
2025-03-06 11:34:28 +0530

    html file

--- MedicalBreakThroughDriver/Controllers/XIB/PrivacyPolicyViewController.swift
+++ MedicalBreakThroughDriver/Controllers/XIB/PrivacyPolicyViewController.swift
@@ -9,13 +9,31 @@ import UIKit
 
 class PrivacyPolicyViewController: UIViewController {
 
+    @IBOutlet weak var textV: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        loadRTFFile()
+        
     }
     @IBAction func backBtnAct(_ sender: UIButton) {
             self.navigationController?.popViewController(animated: true)
 
+    }
+    private func loadRTFFile() {
+        guard let fileURL = Bundle.main.url(forResource: "Driver APP - Privacy Policy", withExtension: "html") else {
+            print("File not found")
+            return
+        }
+        
+        do {
+            let attributedString = try NSAttributedString(
+                url: fileURL,
+                options: [.documentType: NSAttributedString.DocumentType.html],
+                documentAttributes: nil
+            )
+            textV.attributedText = attributedString
+        } catch {
+            print("Failed to load RTF: \(error)")
+        }
     }
 }
