009a7bf79acdc5dfb732220c31d6eb43e3687006
rajeshlolapu444 <rajeshlolapu@webbaysys.com>
2025-04-02 11:26:54 +0530

    app color changes

--- MedicalBreakThroughDriver/Controllers/VC/LoginViewController.swift
+++ MedicalBreakThroughDriver/Controllers/VC/LoginViewController.swift
@@ -40,11 +40,11 @@ class LoginViewController: UIViewController {
         //emailTF.text = "alrickdelivery@medicalbreakthrough.com"
          //emailTF.text = "carlos@medicalbreakthrough.com"
         //emailTF.text = "freddycurbside@medicalbreakthrough.com"
-        //emailTF.text = "guynewhampshire@medicalbreakthrough.com"
+        emailTF.text = "guynewhampshire@medicalbreakthrough.com"
 //        emailTF.text = "rajeshlolapu@gmail.com"
 //        passwordTF.text = "123456"
 //        emailTF.text = "freddyinstall@medicalbreakthrough.com"
-        //passwordTF.text = "apple@123"
+        passwordTF.text = "apple@123"
         configureEyeButton()
     }
     private func configureEyeButton() {
